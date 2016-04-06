9634fbe012e438a227cf15bd568fd46cf00f63b1
mtroysi <morgane.troysi@etud.univ-angers.fr>
2016-04-06 18:07:36 +0200

    Affichage js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $('#user_nbfilms').focus(function(event){
+    event.preventDefault();
+    $('div.books').toggle(true);
+  });
+}); 

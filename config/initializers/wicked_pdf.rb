727e8f0042d6ed1e8ef95310fe333991173a62a3
mtroysi <morgane.troysi@etud.univ-angers.fr>
2016-04-08 22:54:30 +0200

    Modification initializer WickedPDF

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,4 @@
 WickedPdf.config = {
-  exe_path: '/home/etudiant/.rvm/gems/ruby-1.9.3-p551@rails3tutorial/gems/wkhtmltopdf-binary-0.9.9.3/bin/wkhtmltopdf'
+  # exe_path: '/home/etudiant/.rvm/gems/ruby-1.9.3-p551@rails3tutorial/gems/wkhtmltopdf-binary-0.9.9.3/bin/wkhtmltopdf'
   # à remplacer par le chemin correct sur votre machine
 }
